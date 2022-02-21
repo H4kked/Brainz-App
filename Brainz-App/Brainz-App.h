6e81044157eb27bb10c1789cf6dbe9e65955bf06
Mathis <mathis.langrand@gmail.com>
2022-02-21 13:05:02 +0100

    structures

--- Brainz-App/Brainz-App.h
+++ Brainz-App/Brainz-App.h
@@ -7,8 +7,18 @@ struct BRAIN
 	char* name;
 	char* desc;
 	int is_available;
-
+	COMMENT_LIST* list;
 	int note;
+	BRAIN* previous;
+	BRAIN* next;
+};
+
+typedef struct BRAIN_LIST BRAIN_LIST;
+struct BRAIN_LIST
+{
+	int size;
+	BRAIN* first;
+	BRAIN* last;
 };
 
 typedef struct MEMBER MEMBER;
@@ -19,4 +29,32 @@ struct MEMBER
 	char* desc;
 	int is_admin;
 	BRAIN* current_brain;
+};
+
+typedef struct MEMBER_LIST MEMBER_LIST;
+struct MEMBER_LIST
+{
+	int size;
+	MEMBER* first;
+	MEMBER* last;
+};
+
+typedef struct COMMENT COMMENT;
+struct COMMENT
+{
+	MEMBER* member;
+	char* comment;
+	int note;
+	char* date;
+	COMMENT* previous;
+	COMMENT* next;
+};
+
+typedef struct COMMENT_LIST COMMENT_LIST;
+struct COMMENT_LIST
+{
+	int name;
+	int size;
+	COMMENT* first;
+	COMMENT* last;
 };
