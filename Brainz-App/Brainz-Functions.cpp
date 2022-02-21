29531544da0502dcaa0c5857d9205f132b2dcb8c
Mathis <mathis.langrand@gmail.com>
2022-02-21 16:54:39 +0100

    structures dans les .h

--- Brainz-App/Brainz-Functions.cpp
+++ Brainz-App/Brainz-Functions.cpp
@@ -5,57 +5,6 @@
 #include <Windows.h>
 #include "Brainz-App.h"
 
-struct MEMBER
-{
-	char* username;
-	char* password;
-	char* desc;
-	int is_admin;
-	struct BRAIN* current_brain;
-	struct MEMBER* previous;
-	struct MEMBER* next;
-};
-struct MEMBER_LIST
-{
-	int size;
-	struct MEMBER* first;
-	struct MEMBER* logged;
-	struct MEMBER* last;
-};
-struct COMMENT
-{
-	struct MEMBER* member;
-	char* comment;
-	int note;
-	char* date;
-	struct COMMENT* previous;
-	struct COMMENT* next;
-};
-struct COMMENT_LIST
-{
-	int name;
-	int size;
-	struct COMMENT* first;
-	struct COMMENT* last;
-};
-struct BRAIN
-{
-	int id;
-	char* name;
-	char* desc;
-	int is_available;
-	struct COMMENT_LIST* list;
-	int note;
-	struct BRAIN* previous;
-	struct BRAIN* next;
-};
-struct BRAIN_LIST
-{
-	int size;
-	struct BRAIN* first;
-	struct BRAIN* last;
-};
-
 void fMenuDisplay(MEMBER_LIST* list)
 {
 	HWND hwnd = GetForegroundWindow();
